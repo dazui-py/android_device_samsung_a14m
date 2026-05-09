49e60c0890a4c07df1ea500861f0eb21b0677bfb
dazui-py <dazuii@duck.com>
2026-05-09 11:44:57 +0100

    a14m: fix OFRP product config and FBE metadata

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_a14m.mk
+    $(LOCAL_DIR)/ofrp_a14m.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_a14m-user \
-    omni_a14m-userdebug \
-    omni_a14m-eng
+    ofrp_a14m-user \
+    ofrp_a14m-userdebug \
+    ofrp_a14m-eng
